445bc1e2546f3e488a63124d495ada95289ac841
Rob Watson <source.pot.dev@gmail.com>
2024-05-25 20:17:14 +0100

    Team model

---
+++ app/Models/Team.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $id
+ * @property string $name
+ */
+class Team extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $guarded = ['id'];
+}
